a1cc8e92935e2cc14d9a4d200aed73537a08c97c
sanjay <sanj1110@gmail.com>
2016-09-07 08:45:27 -0400

    Initial

---
+++ MyComplexBot/Controllers/MessagesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace MyComplexBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+                string message = activity.Text.ToLower();
+               
+                // return our reply to the user
+               if (message.Contains("hello") || message.Contains("hi"))
+                {
+                    connector.Conversations.ReplyToActivity(activity.CreateReply("Hi"));
+                }
+                else if (message.Contains("create vm") || message.Contains("create virtual machine"))
+                {
+                    connector.Conversations.ReplyToActivity(activity.CreateReply("Virtual machine Creation Succeeded"));
+                }
+                else if (message.Contains("delete vm"))
+                {
+                    connector.Conversations.ReplyToActivity(activity.CreateReply("Virtual machine Deleted Succeeded"));
+                }
+                else
+                {
+                    connector.Conversations.ReplyToActivity(activity.CreateReply("I didnt understand the command"));
+
+                }
+               
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MyComplexBot/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyComplexBot.WebApiApplication" Language="C#" %>
